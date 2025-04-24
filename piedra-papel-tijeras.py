5124b6e59a5ba0f9d49e7b29244a9fea4cc5d508
Sowil86 <97199080+Sowil86@users.noreply.github.com>
2025-04-24 09:21:10 -0300

    Update piedra-papel-tijeras.py
    Realizamos una modificación al código original para mejorar la ejecución y la legibilidad

--- piedra-papel-tijeras.py
+++ piedra-papel-tijeras.py
@@ -1,18 +1,17 @@
-
 nombre1 = input("¿Cómo te llamas jugador n°1? ")
 nombre2 = input("¿Cómo te llamas jugador n°2? ")
 
+eleccion1 = input(f"{nombre1}¿Qué elijes?,¿Piedra, Papel o Tijera?: ").lower()
+eleccion2 = input(f"{nombre2}¿Qué elijes?, ¿Piedra, Papel o Tijera?: ").lower()
 
-nombre1 = input("¿Qué elijes?¿Piedra, Papel o Tijera?: ")
-nombre2 = input("¿Qué elijes?¿Piedra, Papel o Tijera?: ")
+condicion1 = eleccion1 == "piedra" and eleccion2 == "tijera"
+condicion2 = eleccion1 == "papel" and eleccion2 == "piedra"
+condicion3 = eleccion1 == "tijera" and eleccion2 == "papel"
 
-condicion1 = nombre1 == "piedra" and nombre2 == "tijeras"
-condicion2 = nombre1 == "papel" and nombre2 == "piedra"
-condicion3 = nombre1 == "tijeras" and nombre2 == "papel"
-
-if nombre1 == nombre2:
+if eleccion1 == eleccion2:
     print ("¡Ha sido un empate!")
 elif  condicion1 or condicion2 or condicion3:
-    print ("¡Felicidades! Ha ganado: ", nombre1)
+    print (f"{nombre1},¡Felicidades! Ha ganado")
 else:
-    print ("¡Felicidades! Ha ganado: ", nombre2)
+    print (f"{nombre2},¡Felicidades! Ha ganado")
+
